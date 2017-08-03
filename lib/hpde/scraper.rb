3e48f1d990bcc37bb220cf0e5f6613f16517a672
Robert Peckham <peckham.rob@gmail.com>
2017-08-03 15:05:14 +0000

    copied and then saved scraper this time

---
+++ lib/hpde/scraper.rb
@@ -0,0 +1,40 @@
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
+class Scraper
+
+  def initialize
+
+  end
+
+  def self.scrape_calendar(region)
+    dates = []
+    #replace address with argument
+    calendar = Nokogiri::HTML(open("http://hpdejunkie.com/east/"))
+    #iterate over rows
+    calendar.css('table.em-calendar tbody tr').each do |row|
+      #iterate over days
+      row.css('td.eventful a').each do |day|
+        dates << day.text
+      end
+    end
+  end
+
+  def self.scrape_region
+#Hard code this instead. Regions won't change. have cli list regions and use if/else statement to link to chosen region, then start scraping.
+    regions = []
+    home_page = Nokogiri::HTML(open("http://hpdejunkie.com/"))
+    home_page.css('div.columns.three').each do |choose_region|
+      choose_region.css('.button.btn-red').each do |map|
+        regions << map.css('.map-text').text
+      end
+    end
+    # puts "Choose your region"
+    # puts "#{regions.each_with_index}"
+    # region_choice = gets.strip
+  end
+
+  scraper = Scraper.new
+
+end
