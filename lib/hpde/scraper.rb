35276db09f8a38a1df0c72142c3be5071c60eae6
Robert Peckham <peckham.rob@gmail.com>
2017-08-05 17:04:20 +0000

    first part of month_with_tracks working

--- lib/hpde/scraper.rb
+++ lib/hpde/scraper.rb
@@ -8,18 +8,14 @@ class Scraper
 
 
   def initialize
-    @schedule = []
-    @events = []
-
-    day = Nokogiri::HTML(open("http://hpdejunkie.com/events/2017-08-05/?category=48"))
-    #iterate over seperate track events
-    day.css('table.events-table tbody tr td table tr').each do |event|
-      sponsors = event.css('a').text
-      tracks = event.css('i').text
-      events << {sponsor: sponsors, track: tracks}
+    @schedule = [{:date=>"18", :link=>"http://hpdejunkie.com/events/2017-08-18/?category=48"}]
+    @events = [{:sponsor=>"Hooked on Driving", :track=>"VIR, Alton VA"}, {:sponsor=>"Hooked on Driving", :track=>"Sebring International Raceway, Sebring FL"}, {:sponsor=>"TrackDaze", :track=>"VIR, Alton VA"}, {:sponsor=>"Apex Driving Events", :track=>"Palmer Motorsports Park, Ware MA"}, {:sponsor=>"AZP Track Events", :track=>"New York Safety Track, Jefferson NY"}]
+####keep above instance arrays########
+    schedule.each do |day|
+      days_events = scrape_day("#{day[:link]}")
+      binding.pry
     end
-    events.uniq
-    binding.pry
+    # binding.pry
   end
 #to set key(the date) equal to the track(s) with HPDE's for that day (and not just the link)
 #iterate over schedule, for each |day| (day = the hash for a single day)
@@ -28,13 +24,19 @@ class Scraper
   #   scraped_day = scrape_day(Nokogiri::HTML(open(day_value)))
   #
   def month_with_tracks
+    self.schedule.each do |day|
 
+      day.each do |k,v|
+        Nokogiri::HTML(open('v'))
+      end
+
+    end
   end
 
   def scrape_day(day_url)
     #events = []
     #TODO:replace hard code address with argument
-    day = Nokogiri::HTML(open("http://hpdejunkie.com/events/2017-08-05/?category=48"))
+    day = Nokogiri::HTML(open(day_url))
     #iterate over seperate track events
     day.css('table.events-table tbody tr td table tr').each do |event|
       sponsors = event.css('a').text
@@ -44,7 +46,7 @@ class Scraper
     events.uniq
   end
 
-  def scrape_month(region_url)
+  def scrape_month#(region_url)
     #TODO:replace hard code address with argument
     #schedule = []
     calendar_month = Nokogiri::HTML(open("http://hpdejunkie.com/east/"))
