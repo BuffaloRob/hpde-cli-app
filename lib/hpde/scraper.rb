ce7d19bbe55a661515d14712e630edd6f1213aae
Robert Peckham <peckham.rob@gmail.com>
2017-08-05 22:48:22 +0000

    fixed first part ofmonth_with_tracks

--- lib/hpde/scraper.rb
+++ lib/hpde/scraper.rb
@@ -8,40 +8,39 @@ class Scraper
 
 
   def initialize
-    @schedule = [{:date=>"18", :link=>"http://hpdejunkie.com/events/2017-08-18/?category=48"}]
-    @events = [{:sponsor=>"Hooked on Driving", :track=>"VIR, Alton VA"}, {:sponsor=>"Hooked on Driving", :track=>"Sebring International Raceway, Sebring FL"}, {:sponsor=>"TrackDaze", :track=>"VIR, Alton VA"}, {:sponsor=>"Apex Driving Events", :track=>"Palmer Motorsports Park, Ware MA"}, {:sponsor=>"AZP Track Events", :track=>"New York Safety Track, Jefferson NY"}]
-####keep above instance arrays########
+    @schedule = [{"3"=>"http://hpdejunkie.com/events/2017-08-03/?category=48"}, {"6"=>"http://hpdejunkie.com/events/2017-08-06/?category=48"}, {"7"=>"http://hpdejunkie.com/events/2017-08-07/?category=48"}]
+    @days_events = []
+#########################################################################
+    #iterate over each hash in the array @schedule
     schedule.each do |day|
-      days_events = scrape_day("#{day[:link]}")
+      #in each hash run scrape_day(day_url) passing in the link(value) of each hash as the arg
+      day.each_value{|link| @days_events << scrape_day(link)}
       binding.pry
     end
-    # binding.pry
+      # binding.pry
   end
 #to set key(the date) equal to the track(s) with HPDE's for that day (and not just the link)
 #iterate over schedule, for each |day| (day = the hash for a single day)
 #run Nokogiri on the link with the scrape_day method.
   # schedule.each do |day|
-  #   scraped_day = scrape_day(Nokogiri::HTML(open(day_value)))
+  #   days_events = scrape_day(Nokogiri::HTML(open(day_value)))
   #
   def month_with_tracks
-    self.schedule.each do |day|
-
-      day.each do |k,v|
-        Nokogiri::HTML(open('v'))
-      end
-
+    schedule.each do |day|
+      days_events = []
+      days_events << scrape_day("#{day[:link]}")
+      binding.pry
     end
   end
 
   def scrape_day(day_url)
-    #events = []
-    #TODO:replace hard code address with argument
     day = Nokogiri::HTML(open(day_url))
+    events = []
     #iterate over seperate track events
     day.css('table.events-table tbody tr td table tr').each do |event|
       sponsors = event.css('a').text
       tracks = event.css('i').text
-      events << {sponsor: sponsors, track: tracks}
+      events << {sponsors => tracks}
     end
     events.uniq
   end
@@ -58,26 +57,25 @@ class Scraper
         day_number = day.text
         #sets link for that date
         link = day.attr('href')
-        self.schedule << {date: day_number, link: link}
+        self.schedule << {day_number => link}
       end
     end
-
-  end
-
-  def scrape_region
-#Hard code this instead. Regions won't change. have cli list regions and use if/else statement to link to chosen region, then start scraping.
-    regions = []
-    home_page = Nokogiri::HTML(open("http://hpdejunkie.com/"))
-    home_page.css('div.columns.three').each do |choose_region|
-      choose_region.css('.button.btn-red').each do |map|
-        regions << map.css('.map-text').text
-      end
-    end
-    # puts "Choose your region"
-    # puts "#{regions.each_with_index}"
-    # region_choice = gets.strip
   end
 
   scraper = Scraper.new
 
 end
+
+#   def scrape_region
+# #Hard code this instead. Regions won't change. have cli list regions and use if/else statement to link to chosen region, then start scraping.
+#     regions = []
+#     home_page = Nokogiri::HTML(open("http://hpdejunkie.com/"))
+#     home_page.css('div.columns.three').each do |choose_region|
+#       choose_region.css('.button.btn-red').each do |map|
+#         regions << map.css('.map-text').text
+#       end
+#     end
+#     # puts "Choose your region"
+#     # puts "#{regions.each_with_index}"
+#     # region_choice = gets.strip
+#   end
