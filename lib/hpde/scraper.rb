6df8eee5e2db752296f54935973c7cda43b53c48
Robert Peckham <peckham.rob@gmail.com>
2017-08-06 13:37:20 +0000

    month_with_tracks is working!

--- lib/hpde/scraper.rb
+++ lib/hpde/scraper.rb
@@ -3,33 +3,32 @@ require 'nokogiri'
 require 'pry'
 
 class Scraper
-  attr_accessor :schedule, :days_events
+  attr_accessor :date_with_link, :sponsor_and_track, :date_with_track_and_sponsor
 
 
 
   def initialize
-    @schedule = [{"3"=>"http://hpdejunkie.com/events/2017-08-03/?category=48"}, {"6"=>"http://hpdejunkie.com/events/2017-08-06/?category=48"}, {"7"=>"http://hpdejunkie.com/events/2017-08-07/?category=48"}]
-    @days_events = []
+    @date_with_link = [{"3"=>"http://hpdejunkie.com/events/2017-08-03/?category=48"}, {"6"=>"http://hpdejunkie.com/events/2017-08-06/?category=48"}, {"7"=>"http://hpdejunkie.com/events/2017-08-07/?category=48"}]
+    @sponsor_and_track = []
+    @date_with_track_and_sponsor = []
     # = [[{"Apex Driving Events"=>"Lime Rock Park, Lakeville CT"}], [{"Hooked on Driving"=>"VIR, Alton VA"}, {"Rezoom Motorsports"=>"Barber Motorsports Park, Leeds AL"}]]
 #########################################################################
-    #iterate over each hash in the array @schedule
-    self.schedule.each do |day|
+    #iterate over each hash in the array @date_with_link
+    self.date_with_link.each do |day_hash|
       #in each hash run scrape_day(day_url) passing in the link(value) of each hash as the arg
-      #pushes sponsor=>track to days_events array as an array of hashes for each day
-      day.each_value{|link| self.days_events << scrape_day(link)}
-
-      dagy.each_pair
+      #pushes sponsor=>track to sponsor_and_track array as an array of hashes for each day_hash
+      # day_hash.each_value{|link| self.sponsor_and_track << scrape_day(link)}
+      #merge the date in date_with_link with the tracks in sponsor_and_track
+      @date_with_track_and_sponsor << day_hash.each {|date, link| day_hash[date] = scrape_day(link)}
+      binding.pry
     end
-    self.days_events.each do |event|
-      event.each_value
   end
 
 
  # use merge, run scraped_day in the block to replace the old value with the new scraped_day return value
   def month_with_tracks
-    self.schedule.each do |day|
-      #in each hash run scrape_day(day_url) passing in the link(value) of each hash as the arg
-      day.each_value{|link| self.days_events << scrape_day(link)}
+    self.date_with_link.each do |day_hash|
+      @date_with_track_and_sponsor << day_hash.each {|date, link| day_hash[date] = scrape_day(link)}
     end
   end
 
@@ -40,14 +39,14 @@ class Scraper
     day.css('table.events-table tbody tr td table tr').each do |event|
       sponsors = event.css('a').text
       tracks = event.css('i').text
-      events << {sponsors => tracks}
+      events << {sponsor: sponsors, track: tracks}
     end
     events.uniq
   end
 
   def scrape_month#(region_url)
     #TODO:replace hard code address with argument
-    #schedule = []
+    #date_with_link = []
     calendar_month = Nokogiri::HTML(open("http://hpdejunkie.com/east/"))
     #iterate over weeks
     calendar_month.css('table.em-calendar tbody tr').each do |week|
@@ -57,7 +56,7 @@ class Scraper
         day_number = day.text
         #sets link for that date
         link = day.attr('href')
-        self.schedule << {day_number => link}
+        self.date_with_link << {day_number => link}
       end
     end
   end
