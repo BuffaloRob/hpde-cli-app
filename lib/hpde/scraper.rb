1162a3dc4aaa3c0d82563affefbd297c825c22f7
Robert Peckham <peckham.rob@gmail.com>
2017-08-06 14:39:11 +0000

    cleaned up code and added comments

--- lib/hpde/scraper.rb
+++ lib/hpde/scraper.rb
@@ -3,31 +3,23 @@ require 'nokogiri'
 require 'pry'
 
 class Scraper
-  attr_accessor :date_with_link, :sponsor_and_track, :date_with_track_and_sponsor
+  attr_accessor :date_with_link, :date_with_track_and_sponsor
 
 
 
   def initialize
     @date_with_link = [{"3"=>"http://hpdejunkie.com/events/2017-08-03/?category=48"}, {"6"=>"http://hpdejunkie.com/events/2017-08-06/?category=48"}, {"7"=>"http://hpdejunkie.com/events/2017-08-07/?category=48"}]
-    @sponsor_and_track = []
     @date_with_track_and_sponsor = []
-    # = [[{"Apex Driving Events"=>"Lime Rock Park, Lakeville CT"}], [{"Hooked on Driving"=>"VIR, Alton VA"}, {"Rezoom Motorsports"=>"Barber Motorsports Park, Leeds AL"}]]
+
 #########################################################################
-    #iterate over each hash in the array @date_with_link
-    self.date_with_link.each do |day_hash|
-      #in each hash run scrape_day(day_url) passing in the link(value) of each hash as the arg
-      #pushes sponsor=>track to sponsor_and_track array as an array of hashes for each day_hash
-      # day_hash.each_value{|link| self.sponsor_and_track << scrape_day(link)}
-      #merge the date in date_with_link with the tracks in sponsor_and_track
-      @date_with_track_and_sponsor << day_hash.each {|date, link| day_hash[date] = scrape_day(link)}
-      binding.pry
-    end
+
   end
 
 
- # use merge, run scraped_day in the block to replace the old value with the new scraped_day return value
   def month_with_tracks
+    #iterate over each hash in the array @date_with_link
     self.date_with_link.each do |day_hash|
+      #changes the value(link) of the date_with_link hashes to a hash of sponsors and tracks by running scrape_day on the link.
       @date_with_track_and_sponsor << day_hash.each {|date, link| day_hash[date] = scrape_day(link)}
     end
   end
@@ -53,7 +45,7 @@ class Scraper
       #iterate over days
       week.css('td.eventful a').each do |day|
         #sets day_number = date of event
-        day_number = day.text
+        day_str = day.text
         #sets link for that date
         link = day.attr('href')
         self.date_with_link << {day_number => link}
