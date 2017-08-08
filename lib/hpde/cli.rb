c66dc42f3d54edc5a3114e9b294c6afbb239a7bc
Robert Peckham <peckham.rob@gmail.com>
2017-08-08 10:40:11 +0000

    added basic cli functionality/outline

--- lib/hpde/cli.rb
+++ lib/hpde/cli.rb
@@ -1,8 +1,28 @@
 class Hpde::CLI
 
   def call
-    puts "This Months High Performance Driving Events:"
+    select_region
+  end
 
+  def select_region
+    puts "Type the number of the region you would like to view:"
+    puts <<~HEREDOC
+      1. Pacific
+      2. Mountain
+      3. Central
+      4. East
+      HEREDOC
+    #add if/else to select link to specified region
+  end
+
+  def select_month
+    puts "Select the month number you would like to see:"
+    #add way to link to various months
+  end
+
+  def view_month_with_track
+    #call Scraper.scrape_month passing in region_url
+    #call month_with_track to return values you want to see
   end
 
 end
