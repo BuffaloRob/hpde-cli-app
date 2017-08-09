fd1d49e4145edc92ae2b4ef93b812beaba96735b
Robert Peckham <peckham.rob@gmail.com>
2017-08-09 10:13:26 +0000

    added all lib/hpde files to 'require_relative'

--- lib/hpde.rb
+++ lib/hpde.rb
@@ -1,7 +1,10 @@
 require_relative "./hpde/version"
-require_relative "./hpde/scraper"
 require_relative "./hpde/sponsor"
+require_relative "./hpde/date"
+require_relative "./hpde/track"
+require_relative "./hpde/scraper"
 require_relative './hpde/cli'
+# if you run into errors, change the order above cli
 # anything dependent on other files must be below it
 
 module Hpde
