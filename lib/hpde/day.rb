2b1898b6fd5e17216c0097acd2f8f5b7c67a2c65
Robert Peckham <peckham.rob@gmail.com>
2017-08-11 10:24:17 +0000

    changed date class name to day to avoid problems with built in class

---
+++ lib/hpde/day.rb
@@ -0,0 +1,8 @@
+class Hpde::Day
+  attr_accessor :name, :sponsors, :tracks
+
+  # Day.name = "18"
+  # Day.sponsors = ["Hooked on Driving", "NASA HPDE"]
+  # Day.tracks = ["Road Atlanta", "VIR"]
+
+end
