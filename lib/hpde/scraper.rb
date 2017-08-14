d65073937888081fc60167076a7c156f557d7a0d
Robert Peckham <peckham.rob@gmail.com>
2017-08-14 16:25:24 +0000

    replaced scrape_month with select_month and updated initialize accordingly

--- lib/hpde/scraper.rb
+++ lib/hpde/scraper.rb
@@ -5,14 +5,13 @@ require 'pry'
 class Hpde::Scraper
   attr_accessor :date_with_link, :date_with_track_and_sponsor
 
-  def initialize(region_url)
+  def initialize(region_url, month)
     @date_with_link = []
     @date_with_track_and_sponsor = []
-    scrape_month(region_url)
+    select_month(region_url, month)
     month_with_track_and_sponsor
     month_with_track
   end
-
 
   def self.day_input_valid?(chosen_day)
     Hpde::Event.select_day(chosen_day)
@@ -70,12 +69,28 @@ class Hpde::Scraper
     events.uniq
   end
 
-  def scrape_month(region_url)
+  def select_month(region_url, month)
+    month_with_link = []
+    calendar_link = ""
     calendar_month = Nokogiri::HTML(open(region_url))
+    calendar_month.xpath('//*[@id="events-date-dropdown"]/option'). each do |month|
+      month_name = month.text
+      link = month.attr('value')
+      month_with_link << {month_name => link}
+    end
+
+    month_with_link.each do |month_hash|
+      selected_month_hash = month_hash.select {|month_name, link| month_name == month}
+        selected_month_hash.each_value{|link| calendar_link << link}
+    end
+
+    calendar_month = Nokogiri::HTML(open(calendar_link))
     #iterate over weeks
     calendar_month.css('table.em-calendar tbody tr').each do |week|
+      #need to figure out escape sequence, if there aren't any events for that month then it needs to return a message stating that
       #iterate over days
       week.css('td.eventful a').each do |day|
+        # binding.pry
         #sets day_number = date of event
         day_number = day.text
         #sets link for that date
